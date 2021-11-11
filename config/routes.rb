b9a91100855cf9a3a8848ff406abd5711bc841fb
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-11 17:06:56 +0100

    done for the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: %i[index show new create]
+
+  resources :bookmarks, only: %i[new create destroy]
 end
