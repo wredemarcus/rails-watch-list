d3c8341f7d443e7673f09f76c0b8e5c5376d4baf
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-12 12:08:02 +0100

    add bundle lock

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
 
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[show new create]
 
   resources :bookmarks, only: %i[new create destroy]
 
